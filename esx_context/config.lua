9a9aaab57ae8913d3121060897fe406065e1bbdd
CodeForge <125872426+C0deForge@users.noreply.github.com>
2024-01-30 17:17:47 +0100

    Add files via upload

---
+++ esx_context/config.lua
@@ -0,0 +1,13 @@
+PREVIEW_KEYBIND = "LMENU"
+
+Config = { }
+
+Config.Style = {
+    primary = 'linear-gradient(270deg, rgba(255, 0, 102, 0) 0%, rgba(255, 0, 102, 0.9) 64.97%)',
+    secondary = 'linear-gradient(270deg, rgba(79, 79, 79, 0) 0%, rgba(105, 105, 105, 0.384) 98.22%)',
+    background = 'rgba(27, 27, 27, 0.66)',
+    borderSecondary = 'rgba(123, 144, 183, 0.3)',
+    borderPrimary = '#FF0066',
+    unselectable = 'linear-gradient(180deg, rgba(45, 45, 45, 0.225) 0%, rgba(0, 0, 0, 0.9) 100%)',
+    unselectableBorder = '#3D3D3D'
+}
